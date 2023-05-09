a7dc8874ae8c961d3002afaf4bca1ec93a671422
ChristineWest <103592169+Westc13@users.noreply.github.com>
2023-05-09 14:44:28 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted List (javascript)

---
+++ my-folder/problems/remove_duplicates_from_sorted_list/solution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(!head){
+        return head
+    }
+    let current = head.next;
+    let prev = head;
+
+    while (current){
+        if(current.val === prev.val){
+            prev.next = current.next;
+        }
+        else{
+            prev = current;
+        }
+        current = current.next
+    }
+    return head;
+};
