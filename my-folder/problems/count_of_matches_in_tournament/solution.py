f6c5988cb3664d37bbd417c2093b5dba7da42009
ChristineWest <103592169+Westc13@users.noreply.github.com>
2023-07-18 16:43:34 +0000

    Sync LeetCode submission - Count of Matches in Tournament (python3)

--- my-folder/problems/count_of_matches_in_tournament/solution.py
+++ my-folder/problems/count_of_matches_in_tournament/solution.py
@@ -1,6 +1,19 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         return n - 1
-        
-        
-            
+
+
+        # total_matches = 0
+        # while n > 1:
+        #     total_matches += n//2
+        #     n = n//2 + n%2
+        # return total_matches
+
+        # while n > 1:
+        #     if n%2 != 0:
+        #         total_matches += (n - 1)//2 + 1
+        #         n = (n - 1)//2
+        #     else:
+        #         total_matches += (n - 1)//2 + 1
+        #         n = n//2
+        # return total_matches
