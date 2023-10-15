044545ca59bf89bb4e1041b65c3878e6d36ac87a
ChristineWest <103592169+Westc13@users.noreply.github.com>
2023-10-15 13:35:30 +0000

    Sync LeetCode submission - Maximum Nesting Depth of the Parentheses (python3)

---
+++ my-folder/problems/maximum_nesting_depth_of_the_parentheses/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        depth, max_depth = 0, 0
+        for char in s:
+            if char == '(':
+                depth +=1
+                max_depth = max(max_depth, depth)
+            elif char == ')':
+                depth -= 1
+        return max_depth
+                
