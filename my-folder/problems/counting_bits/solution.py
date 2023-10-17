3a2b2ac3d36906a5c346708bd6e4d34603602d8c
ChristineWest <103592169+Westc13@users.noreply.github.com>
2023-10-17 13:06:25 +0000

    Sync LeetCode submission - Counting Bits (python3)

--- my-folder/problems/counting_bits/solution.py
+++ my-folder/problems/counting_bits/solution.py
@@ -1,12 +1,17 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
-        for el in range(int(n+1)):
-            count = 0
-            el_bin = bin(el)[2:]
-            for bit in el_bin:
-                if bit == '1':
-                    count += 1
-            ans.append(count)
+        ans = [0]*(n+1)
+        for i in range(1, n+1):
+            ans[i] = ans[i>>1] + (i & 1)
         return ans
+
+        # ans = []
+        # for el in range(int(n+1)):
+        #     count = 0
+        #     el_bin = bin(el)[2:]
+        #     for bit in el_bin:
+        #         if bit == '1':
+        #             count += 1
+        #     ans.append(count)
+        # return ans
             
