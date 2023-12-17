4fd10cad4e74be5b863a60673db879c8b18fd18b
ChristineWest <103592169+Westc13@users.noreply.github.com>
2023-12-17 14:47:39 +0000

    Sync LeetCode submission - Reformat Date (python3)

---
+++ my-folder/problems/reformat_date/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        months = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+        day, month, year = date.split()
+        day = day[:-2].zfill(2)
+        month = months[month]
+        result_arr = [year, month, day]
+        return '-'.join(result_arr)
