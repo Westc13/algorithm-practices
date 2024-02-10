4ff731274a2dbeb59a924e0a590feb10e18f6654
ChristineWest <103592169+Westc13@users.noreply.github.com>
2024-02-10 11:16:25 +0000

    Sync LeetCode submission - Three Divisors (python3)

---
+++ my-folder/problems/three_divisors/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        divisors = []
+        for i in range(1, n+1):
+            if n % i == 0:
+                divisors.append(i)
+        return True if len(divisors) == 3 else False
