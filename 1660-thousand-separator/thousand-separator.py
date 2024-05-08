c8c4c145a18685bedf02457c518fe0405c1f4be1
ChrisWest <103592169+Westc13@users.noreply.github.com>
2024-05-08 07:25:40 -0400

    Time: 37 ms (37.21%) | Memory: 16.5 MB (48.17%) - LeetSync

--- 1660-thousand-separator/thousand-separator.py
+++ 1660-thousand-separator/thousand-separator.py
@@ -1,9 +1,18 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
-        str_n = str(n)[::-1]
-        result = []
-        for i, digit in enumerate(str_n, 1):
-            result.append(digit)
-            if i % 3 == 0 and i != len(str_n):
-                result.append('.')
-        return ''.join(result[::-1])
+        str_n = str(n)
+        result = ''
+
+        for i in range(len(str_n) - 1, -1, -1):
+            result = str_n[i] + result
+            if(len(str_n) - i) % 3 == 0 and i != 0:
+                result = '.' + result
+        return result
+
+        # str_n = str(n)[::-1]
+        # result = []
+        # for i, digit in enumerate(str_n, 1):
+        #     result.append(digit)
+        #     if i % 3 == 0 and i != len(str_n):
+        #         result.append('.')
+        # return ''.join(result[::-1])
