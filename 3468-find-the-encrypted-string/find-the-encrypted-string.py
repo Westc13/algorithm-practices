cc4b9cc41930e948421ff807ab3ce033d11c1237
ChrisWest <103592169+Westc13@users.noreply.github.com>
2024-07-07 08:34:25 -0400

    Time: 37 ms (100.00%) | Memory: 16.5 MB (100.00%) - LeetSync

---
+++ 3468-find-the-encrypted-string/find-the-encrypted-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getEncryptedString(self, s: str, k: int) -> str:
+        n = len(s)
+        encrypted_string = ''
+        for i in range(n):
+            new_index = (i + k) % n
+            encrypted_string += s[new_index]
+        return encrypted_string
