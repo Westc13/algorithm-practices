ec125e56e2f266879c412d74c7719ea01560bc30
ChrisWest <103592169+Westc13@users.noreply.github.com>
2024-09-28 05:23:57 -0400

    Time: 57 ms (12.70%) | Memory: 16.7 MB (6.40%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,8 +1,11 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        # nums_count = Counter(nums)
+        # result = []
+        # for num, freq in nums_count.items():
+        #     if freq == 2:
+        #         result.append(num)
+        # return result
+
         nums_count = Counter(nums)
-        result = []
-        for num, freq in nums_count.items():
-            if freq == 2:
-                result.append(num)
-        return result
+        return [num for num, freq in nums_count.items() if freq == 2]
