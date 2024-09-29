f4a3d9565699c997f561ba789e923f74029116c5
ChrisWest <103592169+Westc13@users.noreply.github.com>
2024-09-29 09:07:45 -0400

    Time: 37 ms (43.62%) | Memory: 16.5 MB (25.71%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.py
+++ 3567-convert-date-to-binary/convert-date-to-binary.py
@@ -1,6 +1,10 @@
 class Solution:
     def convertDateToBinary(self, date: str) -> str:
-        date = list(date.split('-'))
-        for i in range(len(date)):
-            date[i] = bin(int(date[i]))[2:]
-        return '-'.join(date)
+        # date = list(date.split('-'))
+        # for i in range(len(date)):
+        #     date[i] = bin(int(date[i]))[2:]
+        # return '-'.join(date)
+
+        parts = date.split('-')
+        binary_parts = [bin(int(part))[2:] for part in parts]
+        return '-'.join(binary_parts)
