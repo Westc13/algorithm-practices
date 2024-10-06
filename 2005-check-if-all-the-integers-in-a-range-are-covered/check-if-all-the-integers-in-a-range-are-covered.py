8f1991526e73fbf43346eb20bf00032c8d65d479
ChrisWest <103592169+Westc13@users.noreply.github.com>
2024-10-06 08:50:14 -0400

    Time: 38 ms (87.14%) | Memory: 16.6 MB (7.86%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,11 +1,19 @@
 class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
-        covered = [False] * 51
+        # covered = [False] * 51
+
+        # for start, end in ranges:
+        #     for i in range(start, end + 1):
+        #         covered[i] = True
+        # for i in range(left, right + 1):
+        #     if not covered[i]:
+        #         return False
+        # return True
+
+        needed = set(range(left, right + 1))
 
         for start, end in ranges:
-            for i in range(start, end + 1):
-                covered[i] = True
-        for i in range(left, right + 1):
-            if not covered[i]:
-                return False
-        return True
+            for i in range(max(start, left), min(end, right) + 1):
+                if i in needed:
+                    needed.remove(i)
+        return len(needed) == 0
