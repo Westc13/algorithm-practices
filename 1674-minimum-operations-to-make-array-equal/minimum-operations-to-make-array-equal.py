423724021b873735be3f7622e2aa920397a6b58f
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-04-06 09:49:12 -0400

    Time: 59 ms (14.89%) | Memory: 18 MB (16.01%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.py
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.py
@@ -1,3 +1,12 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        return (n // 2) * ((n + 1) // 2)
+        # return (n // 2) * ((n + 1) // 2)
+
+        arr = [(2 * i) + 1 for i in range(n)]
+        target = n
+        ops = 0
+
+        for num in arr:
+            if num < target:
+                ops += target - num
+        return ops
