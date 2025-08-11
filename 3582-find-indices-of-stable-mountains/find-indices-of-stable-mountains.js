1c3d78b953d384899c0b2f43d20a89b2b5fdb0a9
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-08-11 06:37:00 -0400

    Time: 1 ms (56.00%) | Memory: 57.1 MB (43.00%) - LeetSync

---
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @param {number} threshold
+ * @return {number[]}
+ */
+var stableMountains = function(height, threshold) {
+    const res = [];
+    for (let i = 1; i < height.length; i++){
+        if (height[i - 1] > threshold){
+            res.push(i)
+        }
+    }
+    return res
+};
