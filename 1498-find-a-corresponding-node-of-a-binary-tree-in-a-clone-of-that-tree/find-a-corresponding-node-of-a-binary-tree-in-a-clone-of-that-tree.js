b69cf890c016191d93af66e1227484eab493686c
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-08-23 06:53:02 -0400

    Time: 251 ms (32.94%) | Memory: 78.2 MB (38.82%) - LeetSync

---
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function(original, cloned, target) {
+    if (original === null) return null;
+
+    if (original === target) return cloned;
+
+    let leftResult = getTargetCopy(original.left, cloned.left, target);
+    if (leftResult !== null) return leftResult;
+
+    return getTargetCopy(original.right, cloned.right, target);
+};
