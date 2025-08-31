b93eed4a569dbec65c720dfc1a287ad68878caa5
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-08-31 08:48:35 -0400

    Time: 1 ms (78.16%) | Memory: 55.7 MB (60.92%) - LeetSync

--- 1651-shuffle-string/shuffle-string.js
+++ 1651-shuffle-string/shuffle-string.js
@@ -4,11 +4,16 @@
  * @return {string}
  */
 var restoreString = function(s, indices) {
-    let pairs = [];
+    /**let pairs = [];
     for (let i = 0; i < s.length; i++){
         pairs.push([s[i], indices[i]]);
     }
     pairs.sort((a, b) => a[1] - b[1]);
 
-    return pairs.map(pair => pair[0]).join('');
+    return pairs.map(pair => pair[0]).join('');*/
+    let res = new Array(s.length);
+    for (let i = 0; i < s.length; i++){
+        res[indices[i]] = s[i];
+    }
+    return res.join('')
 };
