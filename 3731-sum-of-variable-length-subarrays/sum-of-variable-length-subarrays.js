5e48fbc5cbf9ce7c5b5ff5c0b731835641dc0f34
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-09-01 07:59:19 -0400

    Time: 0 ms (100.00%) | Memory: 55.4 MB (86.59%) - LeetSync

--- 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.js
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var subarraySum = function(nums) {
-    let res = 0;
+    /*let res = 0;
     const subArr = [];
     for (let i = 0; i < nums.length; i++){
         let start = Math.max(0, i - nums[i]);
@@ -11,5 +11,21 @@ var subarraySum = function(nums) {
             res += nums[j]
         }
     }
-    return res
+    return res */
+
+    let n = nums.length;
+    let prefix = new Array(n).fill(0);
+
+    prefix[0] = nums[0];
+    for (let i = 1; i < n; i++){
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+
+    let res = 0;
+    for (let i = 0; i < n; i++){
+        let start = Math.max(0, i - nums[i]);
+        let sum = prefix[i] - (start > 0 ? prefix[start-1] : 0);
+        res += sum;
+    }
+    return res;
 };
