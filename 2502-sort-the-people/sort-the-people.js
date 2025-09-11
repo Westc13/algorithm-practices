d38e5d503a004e4f0d5bab14ef8ce5da9c0bb7b8
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-09-11 08:09:16 -0400

    Time: 5 ms (90.65%) | Memory: 58.1 MB (95.32%) - LeetSync

--- 2502-sort-the-people/sort-the-people.js
+++ 2502-sort-the-people/sort-the-people.js
@@ -4,12 +4,18 @@
  * @return {string[]}
  */
 var sortPeople = function(names, heights) {
-    const res = [];
+    /* const res = [];
     while (heights.length > 0) {
         let maxIndex = heights.indexOf(Math.max(...heights));
         res.push(names[maxIndex]);
         heights.splice(maxIndex, 1);
         names.splice(maxIndex, 1);
     }
-    return res
+    return res */
+    const people = names.map((name, i) =>
+        [heights[i], name]);
+
+    people.sort((a, b) => b[0] - a[0]);
+
+    return people.map(person => person[1])
 };
