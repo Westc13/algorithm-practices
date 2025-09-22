9b6f16db95e2d88d857647c4072f129ad24160a6
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-09-22 07:58:20 -0400

    Time: 0 ms (100.00%) | Memory: 54.3 MB (67.84%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    let res = true;
+    for (let letter of alphabet){
+        if (!sentence.includes(letter)){
+            res = false;
+            break;
+        }
+    }
+    return res;
+};
