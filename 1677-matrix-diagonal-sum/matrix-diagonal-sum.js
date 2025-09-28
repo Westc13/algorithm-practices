51720d3fe349f3dc143a8077d6c89d02dfbd1bad
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-09-28 09:22:29 -0400

    Time: 1 ms (18.43%) | Memory: 55.9 MB (14.12%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -7,10 +7,24 @@ var diagonalSum = function(mat) {
     let n = mat.length;
 
     for (let i = 0; i < n; i++){
+        for (let j = 0; j < n; j++){
+            if (i === j){
+                res += mat[i][j];
+            }
+            else if (i + j === n - 1){
+                res += mat[i][j];
+            }
+        }
+    }
+    return res;
+    /* let res = 0;
+    let n = mat.length;
+
+    for (let i = 0; i < n; i++){
         res += mat[i][i];
         if (i !== n - 1 - i){
             res += mat[i][n - 1 - i];
         }
     }
-    return res
+    return res */
 };
