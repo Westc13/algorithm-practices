5109d29ef00aaff3d5417f85d956ac5c762c88fe
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-10-02 10:42:40 -0400

    Time: 0 ms (100.00%) | Memory: 55.4 MB (44.58%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.js
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.js
@@ -7,7 +7,7 @@ var pivotInteger = function(n) {
     const x = Math.sqrt(total);
     return Number.isInteger(x) ? x: -1; */
 
-    const total = n * (n + 1) / 2;
+    /* const total = n * (n + 1) / 2;
 
     let leftSum = 0;
 
@@ -20,5 +20,25 @@ var pivotInteger = function(n) {
             return x;
         }
     }
-    return -1;
+    return -1; */
+
+    if (n === 1) return 1;
+
+    let l = 1, r = n;
+    let sumL = 1, sumR = n;
+
+    while (l < r){
+        if (sumL < sumR){
+            l++;
+            sumL += l;
+        } else if (sumR < sumL){
+            r--;
+            sumR += r;
+        } else {
+            l++;
+            sumL += l;
+        }
+    }
+    return (sumL === sumR) ? l : -1;
+    
 };
