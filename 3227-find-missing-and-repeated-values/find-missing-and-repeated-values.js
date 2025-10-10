322766644df157fbb8029906e23edaff6dd0118b
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-10-10 08:45:43 -0400

    Time: 0 ms (100.00%) | Memory: 56.3 MB (89.37%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.js
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 var findMissingAndRepeatedValues = function(grid) {
-    const n = grid.length;
+    /* const n = grid.length;
     const total = n * n;
     const freq = new Array(total + 1).fill(0);
 
@@ -21,5 +21,31 @@ var findMissingAndRepeatedValues = function(grid) {
     for (let k = 1; k <= total; k++){
         if (freq[k] === 0){ missing = k; break;}
     }
-    return [repeated, missing]
+    return [repeated, missing] */
+
+    const n = grid.length;
+    const m = n * n;
+
+    let actualSum = 0;
+    let actualSqSum = 0;
+
+    for (let i = 0; i < n; i++){
+        for (let j = 0; j < n; j++){
+            const v = grid[i][j];
+            actualSum += v;
+            actualSqSum += v * v;
+        }
+    }
+
+    const sumExpected = (m * (m + 1)) / 2;
+    const sqExpected = (m * (m + 1) * (2 * m + 1)) / 6;
+
+    const S = sumExpected - actualSum;
+    const T = sqExpected - actualSqSum;
+
+    const sumBA = T / S;
+    const b = (S + sumBA) / 2;
+    const a = b - S;
+
+    return [Math.round(a), Math.round(b)];
 };
