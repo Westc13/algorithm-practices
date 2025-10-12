43b507c2b3e735925af910d78e785d93fd2a9d35
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-10-12 08:28:51 -0400

    Time: 0 ms (100.00%) | Memory: 55.2 MB (71.96%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.js
+++ 822-unique-morse-code-words/unique-morse-code-words.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var uniqueMorseRepresentations = function(words) {
-    const transformations = new Set();
+    /* const transformations = new Set();
     const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
 
     for (let word of words){
@@ -13,5 +13,26 @@ var uniqueMorseRepresentations = function(words) {
         }
         transformations.add(transformation)
     }
-    return transformations.size
+    return transformations.size */
+
+    const mapping = {
+        'a': ".-",  'b': "-...", 'c': "-.-.", 'd': "-..",
+        'e': ".",   'f': "..-.", 'g': "--.",  'h': "....",
+        'i': "..",  'j': ".---", 'k': "-.-",  'l': ".-..",
+        'm': "--",  'n': "-.",   'o': "---",  'p': ".--.",
+        'q': "--.-",'r': ".-.",  's': "...",  't': "-",
+        'u': "..-", 'v': "...-", 'w': ".--",  'x': "-..-",
+        'y': "-.--",'z': "--.."
+    };
+
+    const transformations = new Set();
+
+    for (let word of words){
+        let transformation = '';
+        for (let ch of word){
+            transformation += mapping[ch];
+        }
+        transformations.add(transformation);
+    }
+    return transformations.size;
 };
