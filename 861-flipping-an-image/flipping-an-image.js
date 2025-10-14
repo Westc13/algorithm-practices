845835f4e8931a944d8ebd11479732e24a23e437
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-10-14 07:47:05 -0400

    Time: 2 ms (27.94%) | Memory: 57.7 MB (35.57%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.js
+++ 861-flipping-an-image/flipping-an-image.js
@@ -3,11 +3,28 @@
  * @return {number[][]}
  */
 var flipAndInvertImage = function(image) {
-    for (let grid of image){
+    /* for (let grid of image){
         for (let i = 0; i < grid.length; i++){
             grid[i] = grid[i] ^ 1;
         }
         grid.reverse()
+    }
+    return image; */
+
+    for (let grid of image) {
+        const n = grid.length;
+        for (let i = 0; i < Math.ceil(n / 2); i++) {
+            let j = n - i - 1;
+            if (i === j) {
+                grid[i] = grid[j] ^ 1;
+            }
+            else {
+                const left = grid[i];
+                const right = grid[j];
+                grid[i] = right ^ 1;
+                grid[j] = left ^ 1;
+            }
+        }
     }
     return image;
 };
