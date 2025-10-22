7543ff55c58c9aab46331be7565f103c99b6ed04
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-10-22 06:37:28 -0400

    Time: 44 ms (96.46%) | Memory: 57.6 MB (31.82%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.js
@@ -3,7 +3,20 @@
  * @return {number}
  */
 var maxProductDifference = function(nums) {
-    const numsSorted = nums.sort((a, b) => a - b);
+    /* const numsSorted = nums.sort((a, b) => a - b);
     const n = nums.length;
-    return (nums[n-1] * nums[n-2]) - (nums[0] * nums[1]);
+    return (nums[n-1] * nums[n-2]) - (nums[0] * nums[1]); */
+
+    let max1 = -Infinity, max2 = -Infinity;
+    let min1 = Infinity, min2 = Infinity;
+
+    for (const x of nums) {
+        if (x > max1) { max2 = max1; max1 = x; }
+        else if (x > max2) { max2 = x; }
+
+        if (x < min1) { min2 = min1; min1 = x; }
+        else if (x < min2) { min2 = x; }
+    }
+
+    return (max1 * max2) - (min1 * min2);
 };
