e50ca57574e50ed8f6695dd9fafeb78ac74f17a0
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-10-26 08:37:57 -0400

    Time: 1 ms (89.63%) | Memory: 58 MB (64.02%) - LeetSync

--- 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.js
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.js
@@ -4,11 +4,13 @@
  * @return {boolean}
  */
 var isAcronym = function(words, s) {
-    let acronym = '';
+    /* let acronym = '';
     for (let word of words) {
         acronym += (word[0]);
     }
     if (acronym !== s) {
         return false;
-    } else {return true}
+    } else {return true} */
+
+    return words.map(w => w[0]).join('') === s;
 };
