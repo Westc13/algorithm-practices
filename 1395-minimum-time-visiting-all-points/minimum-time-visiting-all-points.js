776a211453df311fc7b6c1b4bfa7ef3f7de007df
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-10-29 20:11:39 -0400

    Time: 2 ms (47.24%) | Memory: 58.3 MB (14.17%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    let ans = 0;
+    for (let i = 0; i < points.length - 1; i++) {
+        const [x1, y1] = points[i];
+        const [x2, y2] = points[i+1];
+        const dx = Math.abs(x2 - x1);
+        const dy = Math.abs(y2 - y1);
+        ans += Math.max(dx, dy);
+    }
+    return ans;
+};
