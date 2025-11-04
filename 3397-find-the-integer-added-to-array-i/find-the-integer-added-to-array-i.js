6cdf5241250640578fd29db05116b29575fcdf11
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-04 06:52:07 -0500

    Time: 0 ms (100.00%) | Memory: 55.8 MB (60.71%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.js
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.js
@@ -8,7 +8,11 @@ var addedInteger = function(nums1, nums2) {
     const nums2Sorted = nums2.sort((a,b) => a - b);
     return (nums2Sorted[0] - nums1Sorted[0]); */
 
-    const nums1Min = Math.min(...nums1);
+    /* const nums1Min = Math.min(...nums1);
     const nums2Min = Math.min(...nums2);
-    return nums2Min - nums1Min;
+    return nums2Min - nums1Min; */
+
+    const sum1 = nums1.reduce((accu, curr) => accu + curr, 0);
+    const sum2 = nums2.reduce((accu, curr) => accu + curr, 0);
+    return (sum2 - sum1)/nums1.length;
 };
