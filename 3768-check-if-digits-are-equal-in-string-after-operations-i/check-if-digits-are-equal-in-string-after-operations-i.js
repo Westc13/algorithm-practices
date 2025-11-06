bd8639150349e21ce3b9360b97eb1c236adf75d8
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-06 14:12:07 -0500

    Time: 10 ms (53.79%) | Memory: 57.8 MB (81.08%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.js
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var hasSameDigits = function(s) {
-    let digits = s.split('').map(Number);
+    /* let digits = s.split('').map(Number);
     let alteredD = [];
     while (digits.length > 2) {
         for (let i = 0; i < digits.length - 1; i++) {
@@ -12,6 +12,22 @@ var hasSameDigits = function(s) {
         }
         digits = alteredD;
         alteredD = [];
+    }
+    return digits[0] === digits[1]; */
+
+    let digits = s.split('').map(Number);
+    let len = digits.length;
+    let buf = new Array(len - 1).fill(0);
+    while (len > 2) {
+        let writeCount = len - 1;
+        for (let i = 0; i < writeCount; i++) {
+            buf[i] = (digits[i] + digits[i+1]) % 10;
+
+        }
+        let temp = digits;
+        digits = buf;
+        buf = temp;
+        len = writeCount;
     }
     return digits[0] === digits[1];
 };
