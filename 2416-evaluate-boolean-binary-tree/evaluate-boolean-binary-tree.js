ee54ea09d81ab9e9c51489f99ea6e9c4baac0674
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-10 18:23:02 -0500

    Time: 1 ms (44.29%) | Memory: 60.9 MB (44.29%) - LeetSync

---
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function(root) {
+    if (root.left === null && root.right === null) {
+        return root.val === 1;
+    } 
+    const leftVal = evaluateTree(root.left);
+    const rightVal = evaluateTree(root.right);
+
+    if (root.val === 2) {
+        return leftVal || rightVal;
+    }
+    else if (root.val === 3) {
+        return leftVal && rightVal;
+    }
+};
