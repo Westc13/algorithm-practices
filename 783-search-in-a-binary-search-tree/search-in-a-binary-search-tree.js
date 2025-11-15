8649c1bd8820f6a54c322150389762018e2aabbf
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-15 18:55:04 -0500

    Time: 0 ms (100.00%) | Memory: 63 MB (57.46%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -12,9 +12,16 @@
  * @return {TreeNode}
  */
 var searchBST = function(root, val) {
-    if (root === null) return null;
+    /* if (root === null) return null;
     if (root.val === val) return root;
         
     if (val < root.val) return searchBST(root.left, val);
-    return searchBST(root.right, val);
+    return searchBST(root.right, val); */
+
+    let current = root;
+    while (current !== null) {
+        if (current.val === val) return current;
+        current = (val < current.val) ? current.left : current.right;
+    }
+    return null;
 };
