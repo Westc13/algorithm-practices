ca46b0a6fe45cab1d197b492f3992fbed2734770
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-17 20:13:11 -0500

    Time: 1 ms (31.56%) | Memory: 52.6 MB (97.79%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
@@ -3,7 +3,21 @@
  * @return {number}
  */
 var totalMoney = function(n) {
-    let fullWeeks = Math.floor(n/7);
+    /* let fullWeeks = Math.floor(n/7);
     let remainder = n % 7;
-    return 28 * fullWeeks + 7 * (fullWeeks * (fullWeeks - 1) / 2) + remainder * (1 + fullWeeks) + remainder * (remainder - 1) / 2
+    return 28 * fullWeeks + 7 * (fullWeeks * (fullWeeks - 1) / 2) + remainder * (1 + fullWeeks) + remainder * (remainder - 1) / 2 */
+    let total = 0;
+    let currentMonday = 1;
+    let dayIndexInWeek = 0;
+    let currentDeposit = currentMonday + dayIndexInWeek;
+    for (let i = 0; i < n; i++) {
+        total += currentDeposit;
+        dayIndexInWeek += 1;
+        if (dayIndexInWeek === 7) {
+            dayIndexInWeek = 0;
+            currentMonday += 1;
+        }
+        currentDeposit = currentMonday + dayIndexInWeek;
+    }
+    return total;
 };
