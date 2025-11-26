c60440e4bb535eb393b5b3d0bc89747e2001cbe9
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-25 19:09:49 -0500

    Time: 55 ms (12.29%) | Memory: 54.9 MB (58.70%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,5 +3,9 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    return Object.keys(obj).length === 0;
+    /* return Object.keys(obj).length === 0; */
+    for (let key in obj) {
+        return false;
+    }
+    return true;
 };
