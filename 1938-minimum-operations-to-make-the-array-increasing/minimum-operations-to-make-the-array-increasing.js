a23a2e311735ee388cf74c04b5077e56c83947e4
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-28 15:09:42 -0500

    Time: 57 ms (31.25%) | Memory: 57.3 MB (44.44%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.js
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.js
@@ -3,12 +3,25 @@
  * @return {number}
  */
 var minOperations = function(nums) {
-    let count = 0;
+    /* let count = 0;
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] <= nums[i-1]) {
             const needed = nums[i-1] + 1 - nums[i]
             count += needed;
             nums[i] += needed;
+        }
+    }
+    return count; */
+
+    if (nums.length <= 1) return 0;
+    let count = 0;
+    let prev = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > prev) {
+            prev = nums[i];
+        } else {
+            prev = prev + 1;
+            count += prev - nums[i]
         }
     }
     return count;
