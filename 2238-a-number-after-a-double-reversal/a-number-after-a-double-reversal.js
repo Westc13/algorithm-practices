828e02a7eb07d63b3a578c18014f75652f4e64fb
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-11-30 09:18:34 -0500

    Time: 0 ms (100.00%) | Memory: 54.2 MB (36.26%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.js
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.js
@@ -3,9 +3,17 @@
  * @return {boolean}
  */
 var isSameAfterReversals = function(num) {
-    let digits = num.toString().split('').map((num) => parseInt(num));
+    /* let digits = num.toString().split('').map((num) => parseInt(num));
     if (digits.at(-1) === 0 && digits.length > 1) {
         return false;
     }
-    return true;
+    return true; */
+
+    let digits = num.toString().split('');
+    let reverse1 = parseInt(digits.reverse().join(''));
+    let reverse2 = parseInt(reverse1.toString().split('').reverse().join(''));
+    if (num === reverse2) {
+        return true
+    }
+    return false;
 };
