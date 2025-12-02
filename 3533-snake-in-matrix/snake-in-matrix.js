e470d8f1f4fe8d8e4c7ff20bdc839d4039cbd106
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-12-02 17:00:00 -0500

    Time: 1 ms (79.45%) | Memory: 57.7 MB (21.92%) - LeetSync

--- 3533-snake-in-matrix/snake-in-matrix.js
+++ 3533-snake-in-matrix/snake-in-matrix.js
@@ -4,12 +4,22 @@
  * @return {number}
  */
 var finalPositionOfSnake = function(n, commands) {
-    let idx = 0;
+    /* let idx = 0;
     for (const cmd of commands) {
         if (cmd === 'RIGHT') idx += 1;
         else if (cmd === 'LEFT') idx -= 1;
         else if (cmd === 'DOWN') idx += n;
         else if (cmd === 'UP') idx -= n;
     }
-    return idx;
+    return idx; */
+
+    let row = 0, col = 0;
+
+    for (const cmd of commands) {
+        if (cmd === 'RIGHT') col += 1;
+        else if (cmd === 'LEFT') col -= 1;
+        else if (cmd === 'DOWN') row += 1;
+        else if (cmd === 'UP') row -= 1;
+    }
+    return row * n + col;
 };
