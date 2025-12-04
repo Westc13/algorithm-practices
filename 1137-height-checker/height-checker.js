d4a5232c4dd169f17507835b299afbb1f8093a00
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-12-04 11:23:33 -0500

    Time: 1 ms (54.47%) | Memory: 55.4 MB (26.17%) - LeetSync

--- 1137-height-checker/height-checker.js
+++ 1137-height-checker/height-checker.js
@@ -3,10 +3,29 @@
  * @return {number}
  */
 var heightChecker = function(heights) {
-    let heightsSorted = [...heights].sort((a, b) => a - b);
+    /* let heightsSorted = [...heights].sort((a, b) => a - b);
     let count = 0;
     for (let i = 0; i < heights.length; i++) {
         if (heightsSorted[i] !== heights[i]) count ++;
     }
-    return count;
+    return count; */
+
+    const count = new Array(101).fill(0);
+
+    for (let h of heights) {
+        count[h]++;
+    }
+    let index = 0;
+    let mismatches = 0;
+
+    for (let height = 1; height <= 100; height++) {
+        while (count[height] > 0) {
+            if (heights[index] !== height) {
+                mismatches++;
+            }
+            index++;
+            count[height]--;
+        }
+    }
+    return mismatches;
 };
