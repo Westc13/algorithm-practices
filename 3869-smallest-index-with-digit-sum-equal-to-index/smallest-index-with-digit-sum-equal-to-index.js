67fd8dcf518d283033c95cec8ed0fc150e45cf8b
ChrisWest <103592169+Westc13@users.noreply.github.com>
2025-12-29 09:47:32 -0500

    Time: 5 ms (8.93%) | Memory: 58.1 MB (10.71%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.js
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var smallestIndex = function(nums) {
-    let smallestIdx = Infinity;
+    /* let smallestIdx = Infinity;
     for (let i = 0; i < nums.length; i++) {
         let tempNum = nums[i];
         let numSum = 0;
@@ -11,6 +11,17 @@ var smallestIndex = function(nums) {
             numSum += tempNum % 10;
             tempNum = Math.floor(tempNum / 10);
         }
+        if (numSum === i) {
+            smallestIdx = i;
+            break;
+        }
+    }
+    return smallestIdx === Infinity ? -1 : smallestIdx; */
+    
+    let smallestIdx = Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        const digits = nums[i].toString().split('').map(Number);
+        const numSum = digits.reduce((accu, curr) => accu + curr, 0);
         if (numSum === i) {
             smallestIdx = i;
             break;
