273912544a074ae7587da5008c277765c6c05350
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-01-02 09:20:36 -0500

    Time: 73 ms (15.00%) | Memory: 60.1 MB (52.14%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.js
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.js
@@ -11,11 +11,26 @@
  * @return {number[]}
  */
 var postorder = function(root) {
-    if (root === null) return [];
+    /* if (root === null) return [];
     let result = [];
     for (let child of root.children) {
         result.push(...postorder(child));
     }
     result.push(root.val);
-    return result;
+    return result; */
+
+    if (root === null) return [];
+    
+    let result = [];
+    let stack = [root];
+
+    while (stack.length > 0) {
+        let node = stack.pop();
+        result.push(node.val);
+
+        for (let child of node.children) {
+            stack.push(child);
+        }
+    }
+    return result.reverse();
 };
