10cb9dedc945ff8c9dbb57f7f6660fca25d742de
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-01-04 09:37:59 -0500

    Time: 2 ms (51.23%) | Memory: 58 MB (12.58%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maxFrequencyElements = function(nums) {
-    const freq = nums.reduce((accu, curr) => {accu[curr] = (accu[curr] || 0) + 1;
+    /* const freq = nums.reduce((accu, curr) => {accu[curr] = (accu[curr] || 0) + 1;
     return accu;}, {});
 
     const maxFreq = Math.max(...Object.values(freq));
@@ -12,6 +12,27 @@ var maxFrequencyElements = function(nums) {
     for (let count of Object.values(freq)) {
         if (count === maxFreq) {
             total += count;
+        }
+    }
+    return total; */
+
+    const freq = {};
+
+    for (let num of nums) {
+        freq[num] = (freq[num] || 0) + 1;
+    }
+
+    let maxFreq = 0;
+    for (let key in freq) {
+        if (freq[key] > maxFreq) {
+            maxFreq = freq[key];
+        }
+    }
+
+    let total = 0;
+    for (let key in freq) {
+        if (freq[key] === maxFreq) {
+            total += freq[key];
         }
     }
     return total;
