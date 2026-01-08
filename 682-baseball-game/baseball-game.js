e66e915a0543b219e6e6ce33c9b51ecda1a4cad9
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-01-08 08:44:21 -0500

    Time: 6 ms (11.53%) | Memory: 53.8 MB (95.44%) - LeetSync

---
+++ 682-baseball-game/baseball-game.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    const scores = [];
+    for (let i = 0; i < operations.length; i++) {
+        const op = operations[i];
+        if (!isNaN(op)) {
+            scores.push(parseInt(op));
+        }
+        else if (op === '+') {
+            let last = scores[scores.length - 1];
+            let secondLast = scores[scores.length - 2];
+            scores.push(last + secondLast);
+        }
+        else if (op === 'D') {
+            let last = scores[scores.length - 1];
+            scores.push(last * 2);
+        }
+        else if (op === 'C') {
+            scores.pop();
+        }
+    }
+    return scores.reduce((accu, curr) => accu + curr, 0)
+
+};
