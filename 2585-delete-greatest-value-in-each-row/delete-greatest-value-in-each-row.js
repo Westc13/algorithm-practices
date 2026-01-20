cbd55a571767aa25cc6a7270030fcc0cd5eb7380
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-01-20 18:09:19 -0500

    Time: 8 ms (36.08%) | Memory: 55.7 MB (96.91%) - LeetSync

---
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var deleteGreatestValue = function(grid) {
+    let res = 0;
+
+    for (let row of grid) {
+        row.sort((a, b) => a - b);
+    }
+
+    let cols = grid[0].length;
+    for (let j = 0; j < cols; j++) {
+        let colMax = 0;
+        for (let i = 0; i < grid.length; i++) {
+            colMax = Math.max(colMax, grid[i][j]);
+        }
+        res += colMax;
+    }
+    return res;
+};
