615be65ed40d8d1182e5a44dd37097de4fe29a83
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-01-26 08:24:58 -0500

    Time: 2 ms (48.96%) | Memory: 56.5 MB (22.33%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -3,11 +3,18 @@
  * @return {number}
  */
 var findNumbers = function(nums) {
-    let res = 0;
+    /* let res = 0;
     for (num of nums) {
         if (num.toString().length % 2 === 0) {
             res ++;
         }
+    }
+    return res; */
+
+    let res = 0;
+    for (let num of nums) {
+        let digits = Math.floor(Math.log10(num)) + 1;
+        if (digits % 2 === 0) res++;
     }
     return res;
 };
