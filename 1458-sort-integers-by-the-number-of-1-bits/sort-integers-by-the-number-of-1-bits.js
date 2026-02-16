d1338c5320200f2043e55da2be63940b33576269
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-02-16 10:02:28 -0500

    Time: 4 ms (95.92%) | Memory: 56.6 MB (87.76%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.js
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.js
@@ -22,6 +22,8 @@ var sortByBits = function(arr) {
         return bitsA - bitsB;
     }) */
 
+
+
     /* const bitCountMap = new Map();
 
     const bitCount = (num) => {
@@ -41,7 +43,9 @@ var sortByBits = function(arr) {
         return diff === 0 ? a - b: diff;
     }) */
 
-    const bitCount = (num) => {
+
+
+    /* const bitCount = (num) => {
         let count = 0;
         while (num > 0) {
             count += num & 1;
@@ -57,5 +61,24 @@ var sortByBits = function(arr) {
             }
             return a[0] - b[0];
         })
-        .map(pair => pair[1]);
+        .map(pair => pair[1]); */
+
+
+    const bitCount = (num) => {
+        let count = 0;
+        while (num !== 0) {
+            num = num & (num - 1);
+            count ++;
+        }
+        return count;
+    };
+    return arr
+        .map(num => [bitCount(num), num])
+        .sort((a, b) => {
+            if (a[0] === b[0]) {
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        })
+        .map(arr => arr[1]);
 };
