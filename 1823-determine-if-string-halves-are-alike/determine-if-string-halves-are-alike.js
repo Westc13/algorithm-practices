31d8ffc1c41e5bf0ae0cfd18fe16963ba3eec7ce
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-02-25 14:20:00 -0500

    Time: 3 ms (66.94%) | Memory: 53.5 MB (91.74%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
@@ -3,8 +3,18 @@
  * @return {boolean}
  */
 var halvesAreAlike = function(s) {
-    let vowels = 'aeiou';
+    /* let vowels = 'aeiou';
     const first = s.slice(0, s.length / 2).toLowerCase().match(/[aeiou]/gi) || [];
     const second = s.slice(s.length / 2).toLowerCase().match(/[aeiou]/gi) || [];
-    return first.length === second.length;
+    return first.length === second.length; */
+
+    let count = 0;
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    let mid = s.length / 2;
+
+    for (let i = 0; i < mid; i++) {
+        if (vowels.has(s[i])) count++;
+        if (vowels.has(s[i + mid])) count--;
+    }
+    return count === 0;
 };
