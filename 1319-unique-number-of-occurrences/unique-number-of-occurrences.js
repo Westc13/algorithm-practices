fc4e91f402cd9118511f6f3fc48750845335b4ff
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-03-07 11:37:48 -0500

    Time: 2 ms (33.64%) | Memory: 55.7 MB (12.57%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const freq = {};
+    for (const num of arr) {
+        freq[num] = (freq[num] || 0) + 1;
+    }
+    const counts = new Set();
+    for (const value of Object.values(freq)) {
+        if (counts.has(value)) {
+            return false;
+        } else {
+            counts.add(value);
+        }
+    }
+    return true;
+};
