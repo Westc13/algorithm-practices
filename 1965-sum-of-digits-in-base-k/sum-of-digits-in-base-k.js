7f94bc8aebb9db249676a2011ddb3cd9d34afea2
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-03-10 19:28:04 -0400

    Time: 0 ms (100.00%) | Memory: 53.4 MB (65.31%) - LeetSync

--- 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.js
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.js
@@ -4,7 +4,14 @@
  * @return {number}
  */
 var sumBase = function(n, k) {
-    const baseK = n.toString(k);
+    /* const baseK = n.toString(k);
     const digits = baseK.split('').map(Number);
-    return digits.reduce((accu, curr) => accu + curr, 0)
+    return digits.reduce((accu, curr) => accu + curr, 0) */
+
+    let sum = 0;
+    while (n > 0) {
+        sum += n % k;
+        n = Math.floor(n / k);
+    }
+    return sum
 };
