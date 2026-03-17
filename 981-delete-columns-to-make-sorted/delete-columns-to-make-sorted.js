a266ea439209cbdb23daa0bf9a0c4f8f3d0f0ec4
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-03-16 20:17:51 -0400

    Time: 8 ms (38.26%) | Memory: 58.8 MB (49.56%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function(strs) {
+    let count = 0;
+    let rows = strs.length;
+    let cols = strs[0].length;
+
+    for (let j = 0; j < cols; j++) {
+        for (let i = 0; i < rows - 1; i++) {
+            if (strs[i][j] > strs[i+1][j]) {
+                count++;
+                break;
+            }
+        }
+    }
+    return count;
+};
