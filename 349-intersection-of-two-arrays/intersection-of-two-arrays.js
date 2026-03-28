535c2a1d62655968a362cfb95d14897a0fc44db7
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-03-28 10:38:16 -0400

    Time: 0 ms (100.00%) | Memory: 57.2 MB (7.44%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -4,11 +4,21 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    const result = [];
+    /* const result = [];
     for (let num of nums1) {
         if (nums2.includes(num) && !result.includes(num)) {
             result.push(num);
         }
     }
-    return result;
+    return result; */
+
+    const set2 = new Set(nums2);
+    const result = new Set();
+
+    for (let num of nums1) {
+        if (set2.has(num)) {
+            result.add(num);
+        }
+    }
+    return [...result];
 };
