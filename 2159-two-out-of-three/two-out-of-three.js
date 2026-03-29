9b18fe8157f4e30685f4fe298638a19c1e6bdd7c
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-03-29 08:57:36 -0400

    Time: 3 ms (88.89%) | Memory: 58.9 MB (53.70%) - LeetSync

---
+++ 2159-two-out-of-three/two-out-of-three.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @return {number[]}
+ */
+var twoOutOfThree = function(nums1, nums2, nums3) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const set3 = new Set(nums3);
+    const result = new Set();
+
+    for (let num of set1) {
+        if (set2.has(num) || set3.has(num)) {
+            result.add(num);
+        }
+    }
+
+    for (let num of set2) {
+        if (set1.has(num) || set3.has(num)) {
+            result.add(num);
+        }
+    }
+    return [...result];
+};
