e14edc89019bd072d7388926f0e488d346b0f625
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-07 21:11:12 -0400

    Time: 7 ms (69.90%) | Memory: 57.4 MB (68.93%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.js
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.js
@@ -30,7 +30,7 @@ var findKDistantIndices = function(nums, key, k) {
     }
     return Array.from(result).sort((a, b) => a - b); */
 
-    const result = [];
+    /* const result = [];
 
     for (let i = 0; i < nums.length; i++) {
         for (let j = Math.max(0, i - k); j <= Math.min(nums.length - 1, i + k); j++) {
@@ -38,6 +38,33 @@ var findKDistantIndices = function(nums, key, k) {
                 result.push(i);
                 break;
             }
+        }
+    }
+    return result; */
+
+    const intervals = [];
+
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] === key) {
+            let left = Math.max(0, j - k);
+            let right = Math.min(nums.length - 1, j + k);
+            intervals.push([left, right]);
+        }
+    }
+
+    const merged = [];
+    for (let interval of intervals) {
+        if (merged.length === 0 || merged[merged.length - 1][1] < interval[0]) {
+            merged.push(interval);
+        } else {
+            merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], interval[1])
+        }
+    }
+    
+    const result = [];
+    for (let [start, end] of merged) {
+        for (let i = start; i <= end; i++) {
+            result.push(i);
         }
     }
     return result;
