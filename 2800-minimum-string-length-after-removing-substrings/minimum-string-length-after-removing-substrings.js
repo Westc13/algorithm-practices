01ff66ff9287963cb55d8bc80bfe8f1a5998b40f
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-13 21:17:49 -0400

    Time: 3 ms (68.42%) | Memory: 55.9 MB (95.61%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.js
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.js
@@ -10,12 +10,23 @@ var minLength = function(s) {
     }
     return s.length; */
 
-    let prev;
+    /* let prev;
 
     do {
         prev = s;
         s = s.split('AB').join('').split('CD').join('');
     } while (s !== prev);
 
-    return s.length;
+    return s.length; */
+
+    const stack = [];
+
+    for (let char of s) {
+        if (stack.length &&  ((stack[stack.length -1] === 'A' && char === 'B') || (stack[stack.length - 1] === 'C' && char === 'D'))) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length;
 };
