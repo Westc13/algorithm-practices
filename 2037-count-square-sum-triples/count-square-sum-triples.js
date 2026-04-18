16ff4a4d33596aff0d9906818be46c1a3b2fadf6
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-18 11:18:10 -0400

    Time: 0 ms (100.00%) | Memory: 53.3 MB (94.87%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.js
+++ 2037-count-square-sum-triples/count-square-sum-triples.js
@@ -17,7 +17,7 @@ var countTriples = function(n) {
     }
     return count; */
 
-    let count = 0;
+    /* let count = 0;
     let squares = new Set();
     for (let i = 1; i <= n; i++) {
         squares.add(i * i);
@@ -28,6 +28,41 @@ var countTriples = function(n) {
 
             if (squares.has(sum)) {
                 count++;
+            }
+        }
+    }
+    return count; */
+
+    let count = 0;
+
+    function gcd(a, b) {
+        while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    for (let m = 2; m * m <= n; m++) {
+        for (let n2 = 1; n2 < m; n2++) {
+
+            if (gcd(m, n2) !== 1) continue;
+            if ((m - n2) %2 === 0) continue;
+            let a = m * m - n2 * n2;
+            let b = 2 * m * n2;
+            let c = m * m + n2 * n2;
+
+            if (c > n) break;
+
+            let k = 1;
+            while (k * c <= n) {
+                let A = k * a;
+                let B = k * b;
+
+                if (A <= n && B <= n) {
+                    count += 2;
+                }
+                k++;
             }
         }
     }
