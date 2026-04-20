b0222578979d4b2c17115df35e2cd351d9698061
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-20 19:19:42 -0400

    Time: 1 ms (48.44%) | Memory: 55.9 MB (6.25%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.js
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.js
@@ -5,13 +5,29 @@
  * @return {string[]}
  */
 var divideString = function(s, k, fill) {
-    const result = [];
+    /* const result = [];
     for (let i = 0; i < s.length; i += k) {
         let group = s.slice(i, i + k);
         while (group.length < k) {
             group += fill;
         }
         result.push(group)
+    }
+    return result; */
+
+    const result = [];
+
+    for (let i = 0; i < s.length; i += k) {
+        let group = '';
+
+        for (let j = i; j < i + k; j++) {
+            if (j < s.length) {
+                group += s[j];
+            } else {
+                group += fill;
+            }
+        }
+        result.push(group);
     }
     return result;
 };
