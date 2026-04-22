2b71ab62ee125f2f4049c8f1f098b60393da2764
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-21 22:38:02 -0400

    Time: 0 ms (100.00%) | Memory: 53.3 MB (63.44%) - LeetSync

---
+++ 1454-remove-palindromic-subsequences/remove-palindromic-subsequences.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function(s) {
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return 2;
+        }
+        left++;
+        right--;
+    }
+    return 1;
+};
