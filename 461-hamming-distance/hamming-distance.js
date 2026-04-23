80fb1a54271109c46c71b1d6e0ba82de94d3a6df
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-23 09:17:22 -0400

    Time: 1 ms (15.74%) | Memory: 54.8 MB (7.66%) - LeetSync

--- 461-hamming-distance/hamming-distance.js
+++ 461-hamming-distance/hamming-distance.js
@@ -4,11 +4,28 @@
  * @return {number}
  */
 var hammingDistance = function(x, y) {
-    let xor = x ^ y;
+    let distance = 0;
+
+    let xBit = x.toString(2);
+    let yBit = y.toString(2);
+
+    let maxLength = Math.max(xBit.length, yBit.length);
+
+    xBit = xBit.padStart(maxLength, '0');
+    yBit = yBit.padStart(maxLength, '0');
+
+    for (let i = 0; i < maxLength; i++) {
+        if (xBit[i] !== yBit[i]) {
+            distance++;
+        }
+    }
+    return distance;
+    
+    /* let xor = x ^ y;
     let count = 0;
     while (xor !== 0) {
         count += xor & 1;
         xor = xor >> 1;
     }
-    return count;
+    return count; */
 };
