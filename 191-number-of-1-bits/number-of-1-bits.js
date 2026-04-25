ccb2796ec780504699d7e094a79b5b1cff692a69
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-25 16:45:08 -0400

    Time: 0 ms (100.00%) | Memory: 55.4 MB (21.76%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.js
+++ 191-number-of-1-bits/number-of-1-bits.js
@@ -3,12 +3,21 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    let count = 0;
+    /* let count = 0;
     const nBit = n.toString(2);
     for (let i = 0; i < nBit.length; i++) {
         if (nBit[i] === '1') {
             count++;
         }
+    }
+    return count; */
+
+    let count = 0;
+    while (n > 0) {
+        if ((n & 1) === 1) {
+            count++;
+        }
+        n = n >> 1;
     }
     return count;
 };
