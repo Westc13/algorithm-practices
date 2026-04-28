cc3f52b8637bc6d3171296789a9f610222290acf
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-28 07:01:33 -0400

    Time: 0 ms (100.00%) | Memory: 54.1 MB (77.34%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
@@ -3,10 +3,21 @@
  * @return {number}
  */
 var countGoodSubstrings = function(s) {
-    let count = 0;
+    /* let count = 0;
     for (let i = 0; i <= s.length - 3; i++) {
         let subS = s.slice(i, i + 3);
         if (new Set(subS).size === subS.length) {
+            count++;
+        }
+    }
+    return count; */
+
+    let count = 0;
+    for (let i = 0; i <= s.length - 3; i++) {
+        let a = s[i];
+        let b = s[i + 1];
+        let c = s[i + 2];
+        if (a !== b && b !== c && c !== a) {
             count++;
         }
     }
