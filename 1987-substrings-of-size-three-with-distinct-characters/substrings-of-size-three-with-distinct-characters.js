fde8298e187b24181f434db4a3f7d6ed667878aa
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-04-29 08:06:45 -0400

    Time: 4 ms (35.53%) | Memory: 53.6 MB (86.45%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
@@ -12,13 +12,34 @@ var countGoodSubstrings = function(s) {
     }
     return count; */
 
-    let count = 0;
+    /* let count = 0;
     for (let i = 0; i <= s.length - 3; i++) {
         let a = s[i];
         let b = s[i + 1];
         let c = s[i + 2];
         if (a !== b && b !== c && c !== a) {
             count++;
+        }
+    }
+    return count; */
+
+    let count = 0;
+    let freq = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        freq.set(s[i], (freq.get(s[i]) || 0) + 1);
+
+        if (i >= 3) {
+            let leftChar = s[i - 3];
+            freq.set(leftChar, freq.get(leftChar) - 1);
+
+            if (freq.get(leftChar) === 0) {
+                freq.delete(leftChar);
+            }
+        }
+
+        if (i >= 2 && freq.size === 3) {
+            count++
         }
     }
     return count;
