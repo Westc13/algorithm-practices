3806d03e22d2151ced30065e1bb9c21bbaadfbc7
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-02 09:27:38 -0400

    Time: 6 ms (74.81%) | Memory: 66.4 MB (35.11%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.js
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.js
@@ -3,8 +3,14 @@
  * @return {number}
  */
 var findNonMinOrMax = function(nums) {
-    const numsSorted = nums.sort((a, b) => a - b);
+    /* const numsSorted = nums.sort((a, b) => a - b);
     if (numsSorted.length > 2) {
         return numsSorted[1];
-    } return -1;
+    } return -1; */
+
+    const minVal = Math.min(...nums);
+    const maxVal = Math.max(...nums);
+    const newNums = nums.filter(val => val !== minVal && val !== maxVal);
+    const resultIdx = Math.floor(Math.random() * newNums.length);
+    return newNums.length > 0 ? newNums[resultIdx] : -1;
 };
