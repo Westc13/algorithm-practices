92c7be0f8c30194931605024de3628e0310d8844
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-08 08:07:21 -0400

    Time: 1 ms (68.18%) | Memory: 55.3 MB (86.36%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.js
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var distributeCandies = function(n, limit) {
-    let count = 0;
+    /* let count = 0;
     for (let a = 0; a <= limit; a++) {
         for (let b = 0; b <= limit; b++) {
             let c = n - a - b;
@@ -12,6 +12,17 @@ var distributeCandies = function(n, limit) {
             if (c >= 0 && c <= limit) {
                 count++;
             }
+        }
+    }
+    return count; */
+
+    let count = 0;
+    for (let a = 0; a <= limit; a++) {
+        let low = Math.max(0, n - a - limit);
+        let high = Math.min(limit, n - a);
+
+        if (low <= high) {
+            count += high - low + 1;
         }
     }
     return count;
