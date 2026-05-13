3d5394934fab6a3b8f9eb147ba0dc4b8100354a0
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-13 07:30:38 -0400

    Time: 6 ms (24.94%) | Memory: 55.9 MB (92.00%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    const result = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+
+        let found = false;
+        let nextGreater = -1;
+
+        for (let j = 0; j < nums2.length; j++) {
+
+            if (nums2[j] === nums1[i]) {
+                found = true;
+            }
+
+            else if (found && nums2[j] > nums1[i]) {
+                nextGreater = nums2[j];
+                break;
+            }
+        }
+
+        result.push(nextGreater);
+    }
+
+    return result;
+};
