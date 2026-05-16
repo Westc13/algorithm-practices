5a41036da507404c52881758016dd4fe3b95744f
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-16 06:51:57 -0400

    Time: 13 ms (86.51%) | Memory: 63.3 MB (45.24%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.js
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.js
@@ -5,7 +5,7 @@
  * @return {number[]}
  */
 var findXSum = function(nums, k, x) {
-    const result = [];
+    /* const result = [];
 
     for (let i = 0; i <= nums.length - k; i++) {
         const freq = {};
@@ -30,6 +30,44 @@ var findXSum = function(nums, k, x) {
             sum += Number(num) * count;
         }
         result.push(sum);
+    }
+    return result; */
+
+    const result = [];
+    const freq = {};
+
+    for (let i = 0; i < k; i++) {
+        freq[nums[i]] = (freq[nums[i]] || 0) + 1;
+    }
+
+    function getXSum() {
+        const arr = Object.entries(freq);
+
+        arr.sort((a, b) => {
+            if (b[1] !== a[1]) {
+                return b[1] - a[1];
+            }
+            return b[0] - a[0];
+        })
+        let sum = 0;
+
+        for (let i = 0; i < Math.min(x, arr.length); i++) {
+            sum += Number(arr[i][0]) * arr[i][1];
+        }
+        return sum;
+    }
+    result.push(getXSum());
+
+    for (let right = k; right < nums.length; right++) {
+        const left = right - k;
+        freq[nums[left]]--;
+        if (freq[nums[left]] === 0) {
+            delete freq[nums[left]];
+        }
+
+        freq[nums[right]] = (freq[nums[right]] || 0) + 1;
+
+        result.push(getXSum());
     }
     return result;
 };
