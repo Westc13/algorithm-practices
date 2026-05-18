e908c957bb83de73ea145495f0b73d045dea62f8
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-18 08:36:45 -0400

    Time: 3 ms (42.00%) | Memory: 55.9 MB (52.00%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.js
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.js
@@ -3,13 +3,27 @@
  * @return {number}
  */
 var maximumStrongPairXor = function(nums) {
-    let maxXor = 0;
+    /* let maxXor = 0;
     for (let x = 0; x < nums.length - 1; x++) {
         for (let y = x + 1; y < nums.length; y++) {
             if (Math.abs(nums[x] - nums[y]) <= Math.min(nums[x], nums[y])) {
                 let xor = nums[x] ^ nums[y];
                 maxXor = Math.max(maxXor, xor);
             }
+        }
+    }
+    return maxXor; */
+
+    nums.sort((a, b) => a - b);
+
+    let maxXor = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i; j < nums.length; j++) {
+            if (nums[j] > 2 * nums[i]) {
+                break;
+            }
+            maxXor = Math.max(maxXor, nums[i] ^ nums[j])
         }
     }
     return maxXor;
