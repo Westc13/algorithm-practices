269cad83451de38b6ab7e09b8aed343c897d0f06
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-22 09:04:40 -0400

    Time: 1 ms (45.83%) | Memory: 54.3 MB (38.33%) - LeetSync

--- 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.js
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var minMaxDifference = function(num) {
-    const numStr = num.toString();
+    /* const numStr = num.toString();
 
     let maxStr = numStr;
     for (let ch of numStr) { 
@@ -21,6 +21,44 @@ var minMaxDifference = function(num) {
             break;
         }
     }
-    return Number(maxStr) - Number(minStr);
+    return Number(maxStr) - Number(minStr); */
 
+    const str = num.toString();
+
+    let maxTarget = null;
+    for (let ch of str) {
+        if (ch !== '9') {
+            maxTarget = ch;
+            break;
+        }
+    }
+
+    let maxStr = '';
+
+    for (let ch of str) {
+        if (ch === maxTarget) {
+            maxStr += '9';
+        } else {
+            maxStr += ch;
+        }
+    }
+
+    let minTarget = null;
+    for (let ch of str) {
+        if (ch !== '0') {
+            minTarget = ch;
+            break;
+        }
+    }
+
+    let minStr = '';
+
+    for (let ch of str) {
+        if (ch === minTarget) {
+            minStr += '0';
+        } else {
+            minStr += ch;
+        }
+    }
+    return Number(maxStr) - Number(minStr);
 };
