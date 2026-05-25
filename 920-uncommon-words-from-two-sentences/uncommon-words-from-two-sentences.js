02be2a5bf4beed5bd8857687471472de01a4a87d
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-25 08:15:24 -0400

    Time: 2 ms (45.41%) | Memory: 52.8 MB (96.76%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string[]}
+ */
+var uncommonFromSentences = function(s1, s2) {
+    const result = [];
+    const s1Arr = s1.split(' ');
+    const s2Arr = s2.split(' ');
+    for (let word of s1Arr) {
+        if (s1Arr.filter(w => w === word).length === 1 && !s2Arr.includes(word)) {
+            result.push(word);
+        }
+    }
+    for (let word of s2Arr) {
+        if (s2Arr.filter(w => w === word).length === 1 &&!s1Arr.includes(word)) {
+            result.push(word);
+        }
+    }
+    return result;
+};
