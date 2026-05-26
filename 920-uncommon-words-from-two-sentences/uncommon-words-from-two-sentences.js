507a54ecfee3c73666b154676b208d3e47a70ad4
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-26 08:13:38 -0400

    Time: 2 ms (45.00%) | Memory: 52.8 MB (97.22%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.js
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.js
@@ -4,7 +4,7 @@
  * @return {string[]}
  */
 var uncommonFromSentences = function(s1, s2) {
-    const result = [];
+    /* const result = [];
     const s1Arr = s1.split(' ');
     const s2Arr = s2.split(' ');
     for (let word of s1Arr) {
@@ -14,6 +14,22 @@ var uncommonFromSentences = function(s1, s2) {
     }
     for (let word of s2Arr) {
         if (s2Arr.filter(w => w === word).length === 1 &&!s1Arr.includes(word)) {
+            result.push(word);
+        }
+    }
+    return result; */
+
+    const count = {};
+    const words = (s1 + ' ' + s2).split(' ');
+
+    for (let word of words) {
+        count[word] = (count[word] || 0) + 1;
+    }
+
+    const result = [];
+
+    for (let word in count) {
+        if (count[word] === 1) {
             result.push(word);
         }
     }
