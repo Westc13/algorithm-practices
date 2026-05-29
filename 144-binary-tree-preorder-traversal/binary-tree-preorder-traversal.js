555eea5c2741d086fed467e88c68f039061779e7
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-29 08:26:50 -0400

    Time: 0 ms (100.00%) | Memory: 54.4 MB (15.11%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -11,7 +11,7 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
-    const result = [];
+    /* const result = [];
 
     const helper = function(node) {
         if (node === null) return;
@@ -20,5 +20,23 @@ var preorderTraversal = function(root) {
         helper(node.right);
     }
     helper(root);
+    return result; */
+
+    if (root === null) return [];
+    const stack = [root];
+    const result = [];
+
+    while (stack.length !== 0) {
+        let node = stack.pop()
+        result.push(node.val);
+        if (node.right) {
+            stack.push(node.right);
+        }
+        if (node.left) {
+            stack.push(node.left)
+        }
+
+    }
     return result;
+
 };
