292ebf144d2c56cafeca0de3149bde6092bf3670
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-05-31 08:02:29 -0400

    Time: 3 ms (59.74%) | Memory: 56.1 MB (69.64%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.js
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var findLucky = function(arr) {
-    const freq = arr.reduce((acc, curr) => {
+    /* const freq = arr.reduce((acc, curr) => {
         acc[curr] = (acc[curr] || 0) + 1;
         return acc;
     }, {})
@@ -11,6 +11,20 @@ var findLucky = function(arr) {
     for (const [num, count] of Object.entries(freq)) {
         if (Number(num) === count) {
             largest = Math.max(largest, Number(num))
+        }
+    }
+    return largest; */
+
+    const freq = {};
+    for (const num of arr) {
+        freq[num] = (freq[num] || 0) + 1;
+    }
+
+    let largest = -1;
+
+    for (const num in freq) {
+        if (+num === freq[num]) {
+            largest = Math.max(largest, +num);
         }
     }
     return largest;
