01866243efc02a730b724a478e1a2ceecee3405e
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-06-13 07:48:36 -0400

    Time: 57 ms (47.37%) | Memory: 58 MB (31.58%) - LeetSync

--- 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.js
+++ 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var sumOfEncryptedInt = function(nums) {
-    function encrypt(num) {
+    /* function encrypt(num) {
         const digits = String(num);
         let maxDigit = 0;
 
@@ -16,6 +16,31 @@ var sumOfEncryptedInt = function(nums) {
 
     let sum = 0;
 
+    for (const num of nums) {
+        sum += encrypt(num);
+    }
+    return sum; */
+
+    function encrypt(num) {
+        let maxDigit = 0;
+        let temp = num;
+
+        while (temp > 0) {
+            maxDigit = Math.max(maxDigit, temp % 10);
+            temp = Math.floor(temp / 10);
+        }
+
+        let encrypted = 0;
+        temp = num;
+
+        while (temp > 0) {
+            encrypted = encrypted * 10 + maxDigit;
+            temp = Math.floor(temp / 10);
+        }
+        return encrypted;
+    }
+
+    let sum = 0;
     for (const num of nums) {
         sum += encrypt(num);
     }
