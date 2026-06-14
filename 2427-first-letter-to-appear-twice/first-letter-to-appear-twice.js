c6cb2b51133b44992203f7c9c2686eaa2ae108e9
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-06-14 09:28:22 -0400

    Time: 0 ms (100.00%) | Memory: 55.4 MB (5.67%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var repeatedCharacter = function(s) {
+    let earliestIndex = s.length;
+    let answer = '';
+
+    for (let i = 0; i < s.length; i++) {
+        for ( let j = i + 1; j < s.length; j++) {
+            if (s[i] === s[j]) {
+                if (j < earliestIndex) {
+                    earliestIndex = j;
+                    answer = s[i];
+                }
+                break;
+            }
+        }
+    }
+    return answer;
+};
