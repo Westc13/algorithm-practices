99232d77e8cb49fa816aeec41b36a17e239f5778
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-06-22 09:05:40 -0400

    Time: 14 ms (72.73%) | Memory: 54.9 MB (69.09%) - LeetSync

--- 1844-maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.js
+++ 1844-maximum-number-of-balls-in-a-box/maximum-number-of-balls-in-a-box.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var countBalls = function(lowLimit, highLimit) {
-    const boxes = {};
+    /* const boxes = {};
     let maxBalls = 0;
 
     function digitSum(num) {
@@ -21,6 +21,37 @@ var countBalls = function(lowLimit, highLimit) {
 
         boxes[box] = (boxes[box] || 0) + 1;
         maxBalls = Math.max(maxBalls, boxes[box]);
+    }
+    return maxBalls; */
+
+    function digitSum(num) {
+        let sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    }
+
+    const boxes = {};
+    let maxBalls = 0;
+
+    let currentSum = digitSum(lowLimit);
+
+    for (let num = lowLimit; num <= highLimit; num++) {
+        boxes[currentSum] = (boxes[currentSum] || 0) + 1;
+        maxBalls = Math.max(maxBalls, boxes[currentSum]);
+
+        if (num < highLimit) {
+            currentSum++;
+
+            let x = num;
+
+            while (x % 10 === 9) {
+                currentSum -= 9;
+                x = Math.floor(x / 10);
+            }
+        }
     }
     return maxBalls;
 };
