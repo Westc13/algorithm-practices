e851c5fa135efbba02e412417db1932722a9d443
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-06-28 08:29:38 -0400

    Time: 9 ms (67.10%) | Memory: 56.7 MB (42.58%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countLargestGroup = function(n) {
+    const freq = {}
+    function digitSum(num) {
+        let sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        return sum
+    }
+    for (let i = 1; i <= n; i++) {
+        const sum = digitSum(i);
+        freq[sum] = (freq[sum] || 0) + 1;
+    }
+
+    let maxSize = 0;
+    for (const key in freq) {
+        if (freq[key] > maxSize) {
+            maxSize = freq[key];
+        }
+    }
+
+    let count = 0;
+    for (const key in freq) {
+        if (freq[key] === maxSize) {
+            count++;
+        }
+    }
+    return count;
+
+};
