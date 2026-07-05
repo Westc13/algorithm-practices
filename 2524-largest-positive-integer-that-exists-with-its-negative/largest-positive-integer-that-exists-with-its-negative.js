2c6d654be63de9361a85dce1e28aa91a4e698525
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-07-05 12:32:41 -0400

    Time: 25 ms (8.57%) | Memory: 57.7 MB (40.00%) - LeetSync

---
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxK = function(nums) {
+    let largest = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == -nums[j]) {
+                largest = Math.max(largest, Math.abs(nums[i]));
+            }
+        }
+    }
+    return largest === 0 ? -1 : largest;
+};
