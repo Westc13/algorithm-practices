a305fe9b201861fbb3f1f2485636e42ca362e034
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-07-18 07:15:40 -0400

    Time: 45 ms (72.55%) | Memory: 53.4 MB (74.32%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,8 +3,21 @@
  * @return {number}
  */
 var fib = function(n) {
+    /* if (n <= 1) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2); */
+
     if (n <= 1) {
         return n;
     }
-    return fib(n - 1) + fib(n - 2);
+    let prev = 0;
+    let curr = 1;
+
+    for (let i = 2; i <= n; i++) {
+        const next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 };
