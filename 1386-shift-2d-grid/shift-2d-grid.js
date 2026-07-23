d3fe630b69e1d739777048f846fb196740e4aa35
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-07-23 08:33:57 -0400

    Time: 1 ms (99.62%) | Memory: 61 MB (49.12%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+var shiftGrid = function(grid, k) {
+    const m = grid.length;
+    const n = grid[0].length;
+    const total = m * n;
+
+    k %= total;
+
+    const result = Array.from({length: m}, () => Array(n));
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            const oldIndex = i * n + j;
+            const newIndex = (oldIndex + k) % total;
+
+            const newRow = Math.floor(newIndex / n);
+            const newCol = newIndex % n;
+
+            result[newRow][newCol] = grid[i][j];
+        }
+    }
+    return result;
+};
