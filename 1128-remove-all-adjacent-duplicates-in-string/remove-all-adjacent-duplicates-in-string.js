d0a221a76a90876dd7176ccc20d6f60a80223d98
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-07-30 08:52:56 -0400

    Time: 11 ms (71.15%) | Memory: 63.1 MB (49.63%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    let result = [];
+    for (let i = 0; i < s.length; i++) {
+        if (result.length > 0 && result[result.length - 1] === s[i]) {
+            result.pop();
+        } else {
+            result.push(s[i])
+        }
+    }
+    return result.join('');
+};
