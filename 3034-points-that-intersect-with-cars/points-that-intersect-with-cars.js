cc15cbea833bf7f7a260a236d195c1d9db1eb061
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-08-15 07:31:37 -0400

    Time: 2 ms (89.29%) | Memory: 56.1 MB (75.00%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.js
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var numberOfPoints = function(nums) {
-    const points = [];
+    /* const points = [];
     for (let i = 0; i < nums.length; i++) {
         for (let point = nums[i][0]; point <= nums[i][1]; point++) {
             if (!points.includes(point)) {
@@ -11,5 +11,20 @@ var numberOfPoints = function(nums) {
             }
         }
     }
-    return points.length
+    return points.length; */
+
+    const covered = new Array(101).fill(false);
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let point = nums[i][0]; point <= nums[i][1]; point++) {
+            covered[point] = true;
+        }
+    }
+    let result = 0;
+    for (let point = 1; point <= 100; point++) {
+        if (covered[point]) {
+            result++;
+        }
+    }
+    return result;
 }; 
