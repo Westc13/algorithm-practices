97a6b2ed480ce1722aec42939f841da7b0d13017
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-08-16 08:59:11 -0400

    Time: 0 ms (100.00%) | Memory: 53.7 MB (69.23%) - LeetSync

---
+++ 1005-univalued-binary-tree/univalued-binary-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+    if (root === null) {
+        return true;
+    }
+    if (root.left && root.left.val !== root.val) {
+        return false;
+    }
+    if (root.right && root.right.val !== root.val) {
+        return false
+    }
+    const checkLeft = isUnivalTree(root.left);
+    const checkRight = isUnivalTree(root.right);
+    return checkLeft && checkRight;
+};
