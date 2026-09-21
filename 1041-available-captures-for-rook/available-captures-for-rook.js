65a3923f1c27caa5ad03c4fde38efeec1013e670
ChrisWest <103592169+Westc13@users.noreply.github.com>
2026-09-21 08:52:29 -0400

    Time: 0 ms (100.00%) | Memory: 53.4 MB (60.00%) - LeetSync

--- 1041-available-captures-for-rook/available-captures-for-rook.js
+++ 1041-available-captures-for-rook/available-captures-for-rook.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var numRookCaptures = function(board) {
-    let rookRow, rookCol;
+    /* let rookRow, rookCol;
     let result = 0;
 
     for (let row = 0; row < 8; row++) {
@@ -57,5 +57,15 @@ var numRookCaptures = function(board) {
             break;
         }
     }
-    return result;
-};
+    return result; */
+
+    let rookRow, rookCol; let result = 0;
+    for (let row = 0; row < 8; row++) { for (let col = 0; col < 8; col++) { if (board[row][col] === 'R') { rookRow = row; rookCol = col; } } }
+    
+    function checkSquare(square) { if (square === '.') { return 'continue'; } else if (square === 'p') { result++; return 'stop'; } else if (square === 'B') { return 'stop'; } }
+    
+    for (let i = rookCol - 1; i >= 0; i--) { let action = checkSquare(board[rookRow][i]); if (action === 'stop') { break; } }
+    for (let i = rookCol + 1; i < 8; i++) { let action = checkSquare(board[rookRow][i]); if (action === 'stop') { break; } }
+    for (let i = rookRow - 1; i >= 0; i--) { let action = checkSquare(board[i][rookCol]); if (action === 'stop') { break; } }
+    for (let i = rookRow + 1; i < 8; i++) { let action = checkSquare(board[i][rookCol]); if (action === 'stop') { break; } } return result; 
+    };
